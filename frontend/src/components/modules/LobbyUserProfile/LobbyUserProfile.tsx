b43b9e632ae5d85c290a2828342e1a3a84b776d6
kangmin <kangmin5505@naver.com>
2023-01-30 00:04:38 +0900

    [FE] FEAT: lobby page 유저 프로필에 최근전적 버튼 추가

--- frontend/src/components/modules/LobbyUserProfile/LobbyUserProfile.tsx
+++ frontend/src/components/modules/LobbyUserProfile/LobbyUserProfile.tsx
@@ -4,6 +4,7 @@ import Avatar from "../../atoms/Avatar";
 import Board from "../../atoms/Board";
 import Button from "../../atoms/Button";
 import Typography from "../../atoms/Typography";
+import ButtonGroup from "../ButtonGroup";
 
 const LobbyUserProfileStyled = styled(Board).attrs((props) => {
   return {
@@ -21,7 +22,14 @@ const UserInfo = styled(Board).attrs((props) => {
     height: "100%",
     backgroundColor: props.theme.background.middle,
     flexDirection: "column",
+    justifyContent: "center",
   };
+})``;
+
+const Wrapper = styled(Board).attrs({
+  width: "100%",
+  height: "50%",
+  flexDirection: "column",
 })`
   gap: 0.5rem;
 `;
@@ -31,18 +39,31 @@ const LobbyUserProfile = () => {
     <LobbyUserProfileStyled>
       <Avatar width={"10rem"} height={"10rem"} />
       <UserInfo>
-        <Typography fontSize="1.5rem">user1</Typography>
-        <Typography fontSize="1.5rem">2승 2패 (50%)</Typography>
-        <Typography fontSize="1.5rem">1020 점</Typography>
-        <Button
-          width="10vw"
-          height="5vh"
-          boxShadow
-          to="/rank"
-          fontSize="1.5rem"
-        >
-          랭킹
-        </Button>
+        <Wrapper>
+          <Typography fontSize="1.5rem">user1</Typography>
+          <Typography fontSize="1.5rem">2승 2패 (50%)</Typography>
+          <Typography fontSize="1.5rem">1020 점</Typography>
+        </Wrapper>
+        <ButtonGroup height="30%" width="100%" justifyContent="space-between">
+          <Button
+            width="7vw"
+            height="5vh"
+            boxShadow
+            to="/rank"
+            fontSize="1.5rem"
+          >
+            랭킹
+          </Button>
+          <Button
+            width="7vw"
+            height="5vh"
+            boxShadow
+            to="/stat/1"
+            fontSize="1.5rem"
+          >
+            최근전적
+          </Button>
+        </ButtonGroup>
       </UserInfo>
     </LobbyUserProfileStyled>
   );
