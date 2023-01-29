54c83fcca823ff1fe7ea39314c54a3927946d98a
sichoi42 <42.4.sichoi@gmail.com>
2023-01-30 00:09:34 +0900

    [BE] FEAT: app 모듈에 user 모듈 등록 #25

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,6 +6,7 @@ import { ConfigModule } from '@nestjs/config';
 import configuration from './config/configuration';
 import { SessionMiddleware } from './middleware/session.middleware';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './user/user.module';
 import TypeOrmConfigService from './config/typeorm.config';
 
 @Module({
@@ -19,6 +20,7 @@ import TypeOrmConfigService from './config/typeorm.config';
       useClass: TypeOrmConfigService,
     }),
     AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService, SessionMiddleware],
