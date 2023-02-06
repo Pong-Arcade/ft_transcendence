29cbaacb06e101b24a5fffd1268f3b8a051f39c3
kangmin <kangmin5505@naver.com>
2023-02-06 18:42:25 +0900

    [FE] REFACTOR: 로그인 api 리팩토링

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -1,22 +1,3 @@
-import { Dispatch, SetStateAction } from "react";
-import { Location } from "react-router-dom";
-
-export const login = (setIsLoading: Dispatch<SetStateAction<boolean>>) => {
-  setIsLoading(true);
+export const login = () => {
   window.location.replace(`${import.meta.env.VITE_API_URL}/api/auth/login`);
-};
-
-const LoginQueryKey = "isFirstLogin";
-
-interface IFirstLogin {
-  location: Location;
-  setIsFirstLogin: Dispatch<SetStateAction<boolean>>;
-}
-
-export const firstLogin = ({ location, setIsFirstLogin }: IFirstLogin) => {
-  const searchParams = new URLSearchParams(location.search);
-
-  if (searchParams.get(LoginQueryKey) === "true") {
-    setIsFirstLogin(true);
-  }
 };
