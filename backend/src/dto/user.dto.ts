21f1edfe1e3cff7f348f1183ef39b0dd168df544
sichoi42 <42.4.sichoi@gmail.com>
2023-02-14 20:28:10 +0900

    [BE] FEAT: 최초 로그인 정보 추가 #86

--- backend/src/dto/user.dto.ts
+++ backend/src/dto/user.dto.ts
@@ -2,11 +2,12 @@
  * pong 게임 유저의 정보를 저장합니다.
  * avatarUrl은 사용자의 프로필 사진을 저장합니다.
  * email은 사용자의 이메일을 저장합니다.
- * avatarUrl, email 필드는 확장성을 위해 optional로 설정되어 있습니다.
+ * avatarUrl, email, firstLogin 필드는 확장성을 위해 optional로 설정되어 있습니다.
  */
 export class UserDto {
   userId: number; // 유저의 ID
   nickname: string; // 유저의 닉네임
   avatarUrl?: string; // 아바타 이미지 url
   email?: string; // 유저의 이메일
+  firstLogin?: Date; // 유저의 첫 로그인 시간
 }
