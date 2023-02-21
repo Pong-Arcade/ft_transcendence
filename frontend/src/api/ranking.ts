74004ee8743ee8f996fa4832a2207d5b5e6ce324
kangmin <kangmin5505@naver.com>
2023-02-21 19:06:30 +0900

    [FE] FEAT: 랭킹방 필터 기능 및 샘플 데이터로 확인 #102

--- frontend/src/api/ranking.ts
+++ frontend/src/api/ranking.ts
@@ -1,7 +1,13 @@
+import { ERankingFilter, ERankingOrder } from "../hooks/useRanking";
 import { getRequest } from "./axios";
 
 // TODO: 실패 처리
-export const getRankListAPI = async () => {
-  const response = await getRequest("stat/ranking");
+export const getRankListAPI = async (
+  filter: ERankingFilter,
+  order: ERankingOrder
+) => {
+  const response = await getRequest(
+    `stat/ranking/?filter=${filter}&order=${order}`
+  );
   return response.data.rankList;
 };
