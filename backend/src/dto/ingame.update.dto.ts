8ebb2cb1e04582c8f2c58dcfbbcc519f9463699c
bson <bson@student.42seoul.kr>
2023-02-27 21:27:58 +0900

    [BE] 게임 업데이트 DTO 세부 명칭 변경 #147

--- backend/src/dto/ingame.update.dto.ts
+++ backend/src/dto/ingame.update.dto.ts
@@ -2,7 +2,7 @@ import { Coordinate } from '../game/game.interface';
 
 export class InGameUpdateDto {
   ball: Coordinate;
-  paddle1: Coordinate;
-  paddle2: Coordinate;
+  redPaddle: Coordinate;
+  bluePaddle: Coordinate;
 }
 export { Coordinate };
