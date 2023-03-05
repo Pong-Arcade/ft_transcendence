1eb139dfd2b1cd094c58ffc19535e718e12dbae4
junppyo <63575184+junppyo@users.noreply.github.com>
2023-03-05 15:38:31 +0900

    [COMMON] FIX: 기존 접속자가 다른 세션으로 접속시 기존 접속 끊고 에러모달 출력

--- backend/src/chat/chat.geteway.ts
+++ backend/src/chat/chat.geteway.ts
@@ -99,6 +99,12 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
     if (users.has(info.userId)) {
       user = users.get(info.userId);
       this.server.to(user.socketId).emit('otherLogin');
+      if (user.socketId !== client.id) {
+        const tmp = user.gameSocketId;
+        this.handleDisconnect({ id: user.socketId });
+        user = new User(info.userId, info.userName);
+        user.gameSocketId = tmp;
+      }
     } else {
       user = new User(info.userId, info.userName);
     }
